905bce520a65f774043c7451806d0e492d03df14
Stig Murberg <stimurbe@cisco.com>
2011-09-22 10:56:34 +0200

    completed first 10 koans

--- koans/smu/about_objects.rb
+++ koans/smu/about_objects.rb
@@ -2,21 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 class AboutObjects < EdgeCase::Koan
   def test_everything_is_an_object
-    assert_equal __, 1.is_a?(Object)
-    assert_equal __, 1.5.is_a?(Object)
-    assert_equal __, "string".is_a?(Object)
-    assert_equal __, nil.is_a?(Object)
-    assert_equal __, Object.is_a?(Object)
+    assert_equal true, 1.is_a?(Object)
+    assert_equal true, 1.5.is_a?(Object)
+    assert_equal true, "string".is_a?(Object)
+    assert_equal true, nil.is_a?(Object)
+    assert_equal true, Object.is_a?(Object)
   end
 
   def test_objects_can_be_converted_to_strings
-    assert_equal __, 123.to_s
-    assert_equal __, nil.to_s
+    assert_equal "123", 123.to_s
+    assert_equal "", nil.to_s
   end
 
   def test_objects_can_be_inspected
-    assert_equal __, 123.inspect
-    assert_equal __, nil.inspect
+    assert_equal "123", 123.inspect
+    assert_equal "123", nil.inspect
   end
 
   def test_every_object_has_an_id
