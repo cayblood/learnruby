4d31d1e1f98c9be106c88745baa69a2f01e0f224
Carl Youngblood <carl@youngbloods.org>
2011-10-13 13:06:43 +0200

    Chat rails app

---
+++ rails/chat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = 'd884a4797cd7c0d453f2d1a769b0813a8c64c64b174b8a9cc4b17ad9d68a0eafb81f02b327772b33b98c1f6511341fafa8c5e9bde9a2330bed1d624a9001523b'
