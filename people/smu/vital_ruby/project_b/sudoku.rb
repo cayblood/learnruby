f338ef5abf65b98ebbe7caedcf2a903445ee989d
Stig Murberg <stimurbe@cisco.com>
2012-02-08 16:01:38 +0100

    added sodokooo stuffs

---
+++ people/smu/vital_ruby/project_b/sudoku.rb
@@ -0,0 +1,80 @@
+class Board
+  
+  attr_accessor :board
+  
+  def initialize(name)
+    @board = []
+    read_file name
+  end
+  
+  def read_file(name)
+    open(name).each do |line|
+      line.strip.each_char {|c| @board << c }
+    end
+  end
+
+  def get_row(row_index)
+    @board.slice(row_index * 9, 9)
+  end
+  
+  def is_row_valid?(row_index)
+    get_row(row_index).sort.join.eql? "123456789"
+  end
+
+  def get_col(col_offset)
+    return (0..8).map{|i| @board[col_offset + (i * 9)] }
+  end
+
+  def is_col_valid?(col_offset)
+    get_col(col_offset).sort.join.eql? "123456789"
+  end
+  
+  def get_box(box_index)
+    [0,1,2,9,10,11,18,19,20].map{|offset| @board[offset + (box_index * 3) + ()]}
+  end
+  
+  def is_box_valid?(box_index)
+    true
+  end
+  
+end
+
+describe Board do
+  
+  it "it should read a puzzle" do
+    b = Board.new("puzzles/easy1.sud")  
+    b.board.size.should eq(81)
+  end
+
+  it "it should get the first row" do
+    b = Board.new("puzzles/solved.sud")  
+    b.get_row(0).should eq("325618497".chars.to_a)
+  end
+
+  it "it should validate that rows are solved" do
+    b = Board.new("puzzles/solved.sud")  
+    (0..8).each{ |i| b.is_row_valid?(i).should eq(true) }
+  end
+
+  it "it should get the first col" do
+    b = Board.new("puzzles/solved.sud")  
+    b.get_col(2).should eq("564927381".chars.to_a)
+  end
+  
+  it "it should validate that columns are valid" do
+    b = Board.new("puzzles/solved.sud")  
+    (0..8).each{ |i| b.is_col_valid?(i).should eq(true) }
+  end
+
+  it "it should get the first box" do
+    b = Board.new("puzzles/solved.sud")  
+    b.get_box(1).should eq("618924537".chars.to_a)
+    b.get_box(3).should eq("569142837".chars.to_a)
+  end
+
+  it "it should validate that boxes are valid" do
+    b = Board.new("puzzles/solved.sud")  
+    (0..8).each{ |i| b.is_box_valid?(i).should eq(true) }
+  end
+
+end
